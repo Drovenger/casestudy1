3ba1697c0f093b7d9a727d415b4ef81268ee4b2e
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-06-01 16:46:48 +0700

    them mau cho bong, them che do Mang

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let canvas = document.getElementById("mycanvas");
 let score = document.getElementById("score");
+let life = document.getElementById("life");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let c = canvas.getContext("2d");
 let score_get = 0;
+let life_get = 3;
 let bullets = [];
 let targets = [];
 let g;
@@ -44,18 +46,30 @@ canvas.addEventListener("mousemove", function (event) {
     mouse.y = event.clientY;
 });//khi di chuyển chuột thì vị trí chuột sẽ được cập nhật lại
 
+function getRandomHex() {
+    return Math.floor(Math.random() * 255);
+}
+
+function getRandomColor() {
+    let red = getRandomHex();
+    let green = getRandomHex();
+    let blue = getRandomHex();
+    return "rgb(" + red + "," + green + "," + blue + ")";
+}
+
 function Target() { //lớp mục tiêu
     this.x = canvas.width;
     this.y = canvas.height;
     this.radius = Math.random() * 100 + 10;
     this.ang = (canvas.height / 2) / canvas.width;
     this.dx = -Math.random() * 10 - 3;
-    this.dy = 0;
-    this.color = "red";
+    this.color = getRandomColor();
     this.draw = function () {
         c.beginPath();
-        c.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-        c.strokeStyle = this.color;
+        c.arc(this.x, this.y, this.radius, Math.PI * 2, 0, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.strokeStyle = "red";
         c.lineWidth = 1;
         c.stroke();
         c.closePath();
@@ -70,18 +84,21 @@ function Target() { //lớp mục tiêu
         }
         for (let i = 0; i < bullets.length; i++) {
             if (distance(this.x, this.y, bullets[i].x, bullets[i].y) < (this.radius + bullets[i].radius)) {
-                let b = bullets[i];
                 bullets.splice(i, 1);
                 targets.splice(target_index, 1);
-                score_get += 20;
-                continue;
+                score_get += parseInt(100 - this.radius);
+                if (score_get > 1000) {
+                    life_get++;
+                    score_get = score_get - 1000;
+                }
             }
         }
         this.x += this.dx;
         this.y = this.ang * this.x + canvas.height / 4;
         if (this.x < 0) {
             targets.splice(target_index, 1);
-            score_get -= 10;
+            score_get -= parseInt(this.radius);
+            life_get--;
         }
         this.draw();
     }
@@ -95,7 +112,6 @@ function Bullet() {//lớp đạn
     this.dy = Math.sin(bull_start.angle) * velocity;
     this.color = "black";
     this.radius = 10;
-    this.velocity = 5;
     this.gravity = 0.3;
     this.draw = function () {
         c.beginPath();
@@ -159,25 +175,44 @@ function set_score() {
     score.innerHTML = score_get;
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    g.update();
-    set_score();
-    c.beginPath();
-    c.arc(0, canvas.height, 150, 0, 2 * Math.PI, false);
-    c.fillStyle = "black";
-    c.fill();
-    c.closePath();
-    for (let i = 0; i < bullets.length; i++) {
-        bullets[i].update(bullets);
-    }
-    for (let i = 0; i < targets.length; i++) {
-        targets[i].update(bullets, targets);
+function set_life() {
+    life.innerHTML = life_get;
+}
+
+function confirm() {
+    alert("Bạn đã thua!\n" + "Bạn có muốn chơi lại không?");
+    document.location.reload();
+}
+
+function animate() { //tổng hợp hoạt cảnh
+    if (life_get > 0) {
+        requestAnimationFrame(animate);
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        g.update();
+        set_score();
+        set_life();
+        c.beginPath();
+        c.arc(0, canvas.height, 150, 0, 2 * Math.PI, false);
+        c.fillStyle = "black";
+        c.fill();
+        c.closePath();
+        for (let i = 0; i < bullets.length; i++) {
+            bullets[i].update(bullets);
+        }
+        for (let i = 0; i < targets.length; i++) {
+            targets[i].update(bullets, targets);
+        }
+    } else {
+        confirm();
     }
 }
 
 function start() {
+    alert("Chào mừng bạn đến với game bắn bóng!");
+    alert("Bắt đàu bạn sẽ có 3 mạng,\n" +
+        "Mỗi lần bóng bay mất sẽ mất 1 mạng,\n" +
+        "Mỗi khi đạt 1000 điểm sẽ được thưởng 1 mạng");
+    alert("Chúc các bạn vui vẻ!");
     init();
     animate();
 }
